14519e7724fc4c68f7991d82f8647c3d1cdaa6fa
JavierValeroMoreno <lord.darkuness@gmail.com>
2016-05-28 22:40:50 +0200

    Menuñadir

---
+++ Assets/menu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menu : MonoBehaviour {
+
+
+    public void Jugar()
+    {
+        Debug.Log("jugar pulsado");
+    }
+    public void Acercade()
+    {
+        Debug.Log("Acerca de pulsado");
+        Application.LoadLevel(0);
+    }
+    public void Instrucciones()
+    {
+        Debug.Log("Instrucciones Pulsado");
+        Application.LoadLevel(2);
+    }
+}
---
+++ Assets/scipts/Objets/Event.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Event : MonoBehaviour {
+
+    private Eval currentEval;
+    private float change;
+    private Eval lastEval;
+
+    // Use this for initialization
+    void Start() {
+        currentEval = (Eval)((Random.Range(1,40))%4);
+        foreach (int prim in primo)
+            primos.Add(prim);
+        change = Time.time;
+    }
+
+    bool Evaluate(int entrada)
+    {
+        switch (currentEval)
+        {
+            case Eval.Par: return (entrada % 2 == 0) ? true : false;
+
+            case Eval.Impar: return (entrada % 2 != 0) ? true : false;
+
+            case Eval.Raiz: return (Mathf.Sqrt(entrada) == (int)Mathf.Sqrt(entrada));
+                
+            case Eval.Primo: return primos.Contains(entrada);
+               
+        }
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(Time.time >= change + 10f)
+          {
+              lastEval = currentEval;
+              currentEval = (Eval)((Random.Range(1, 40)) % 4);
+            if (currentEval == lastEval)
+                currentEval = (Eval)(((int)currentEval + 1) % 4);
+              change = Time.time;
+            
+          }
+        
+    }
+    public enum Eval
+    {
+        Par, Impar, Primo, Raiz
+    }
+    public int[] primo = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+    public ArrayList primos = new ArrayList();
+}
